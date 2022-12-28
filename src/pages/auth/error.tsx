393f6803185180394d21ae0711aefbc29bd2b016
Ashish <ashishcbscboard@gmail.com>
2022-12-28 21:12:45 +0530

    refactor: modified error page's error message

--- src/pages/auth/error.tsx
+++ src/pages/auth/error.tsx
@@ -5,12 +5,12 @@ import Container from "@components/Container";
 function ErrorPage() {
 	const router = useRouter();
 	return (
-		<Container className="grid h-screen place-items-center">
-			<div className="max-w-xs rounded border px-8 py-6">
+		<Container className="grid h-screen place-items-center px-2">
+			<div className="w-full max-w-xs rounded border px-8 py-6 sm:min-w-[280px]">
 				<div className="flex flex-col gap-6">
-					<p className="text-lg">{router.query.error}</p>
+					<p className="text-lg">Error: {router.query.error}</p>
 					<button
-						className="rounded border px-4 py-2"
+						className="rounded bg-twitter-blue px-4 py-2"
 						onClick={() => router.push("/")}
 					>
 						Home
